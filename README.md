1ae1aeecdabb811dd6231aac077ff00902e139d3
qdc2k <quadecausa@hotmail.com>
2026-05-02 19:01:10 +0200

    v.1.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cinema Mode
+This app allows to easily disable other screens when watching a video.
---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Building Cinema Mode...
+echo Cleaning NuGet cache and restoring packages...
+dotnet nuget locals all --clear
+dotnet publish CinemaMode.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false
+
+copy /y bin\Release\net8.0-windows\win-x64\publish\CinemaMode.exe .
+
+echo.
+echo Cleaning build artifacts...
+rmdir /s /q bin 2>nul
+rmdir /s /q obj 2>nul
+
+echo Done! Your portable app is now here: %~dp0CinemaMode.exe
+pause
